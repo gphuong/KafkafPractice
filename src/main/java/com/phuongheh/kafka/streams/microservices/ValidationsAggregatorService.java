3f4b4d2c1fe499880c62f2f8ff1de7e9e64c327c
Phuong Nguyen <phuong.nguyen@aeris.net>
2023-03-08 22:59:08 +0700

    kafka stream services

--- src/main/java/com/phuongheh/kafka/streams/microservices/ValidationsAggregatorService.java
+++ src/main/java/com/phuongheh/kafka/streams/microservices/ValidationsAggregatorService.java
@@ -1,18 +1,22 @@
 package com.phuongheh.kafka.streams.microservices;
 
 import com.phuongheh.kafka.streams.avro.microservices.Order;
+import com.phuongheh.kafka.streams.avro.microservices.OrderState;
+import com.phuongheh.kafka.streams.avro.microservices.Order.newBuilder;
 import com.phuongheh.kafka.streams.avro.microservices.OrderValidation;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Grouped;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.StreamJoined;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
+import static com.phuongheh.kafka.streams.avro.microservices.OrderState.VALIDATED;
+import static com.phuongheh.kafka.streams.avro.microservices.OrderValidationResult.PASS;
 import static com.phuongheh.kafka.streams.microservices.domain.Schemas.Topics.ORDERS;
 import static com.phuongheh.kafka.streams.microservices.domain.Schemas.Topics.ORDER_VALIDATIONS;
 
@@ -32,7 +36,31 @@ public class ValidationsAggregatorService implements Service {
 
     @Override
     public void start(String bootstrapServers, String stateDir, Properties defaultConfig) {
+        final CountDownLatch startLatch = new CountDownLatch(1);
+        streams = aggregateOrderValidations(bootstrapServers, stateDir, defaultConfig);
+    }
 
+    private KafkaStreams aggregateOrderValidations(String bootstrapServers, String stateDir, Properties defaultConfig) {
+        final int numberOfRules = 3;
+        final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, OrderValidation> validations = builder.stream(ORDER_VALIDATIONS.name(), serdes1);
+        final KStream<String, Order> orders = builder.stream(ORDERS.name(), serdes2)
+                .filter((id, order) -> OrderState.CREATED.equals(order.getState()));
+        validations
+                .groupByKey(serdes3)
+                .windowedBy(SessionWindows.with(Duration.ofMinutes(5)))
+                .aggregate(
+                        () -> 0L,
+                        (id, result, total) -> PASS.equals(result.getValidationResult()) ? total + 1 : total,
+                        (k, a, b) -> b == null ? a : b,
+                        Materialized.with(null, Serdes.Long())
+                ).toStream((windowedKey, total) -> windowedKey.key())
+                .filter((k1, v) -> v != null)
+                .filter((k, total) -> total >= numberOfRules)
+                .join(orders, (id, order) ->
+                                newBuilder(order).setState(VALIDATED).build()
+                        , JoinWindows.of(Duration.ofMinutes(5)), serdes4)
+                .to(ORDERS.name(), serdes5);
     }
 
     @Override
